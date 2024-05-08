bdae7a840194fa48d0b7e6a1b933e6f3ee1b25d1
jhorvitz124 <113337655+jhorvitz124@users.noreply.github.com>
2024-05-08 09:47:21 -0400

    Create ClientCode.py

---
+++ ClientCode.py
@@ -0,0 +1,31 @@
+import socket
+import cv2
+import pickle
+import struct
+
+# create socket
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+host_ip = '192.168.0.104' # Enter your server's IP address here
+port = 9999
+client_socket.connect((host_ip,port)) 
+data = b""
+payload_size = struct.calcsize("Q")
+while True:
+    while len(data) < payload_size:
+        packet = client_socket.recv(4*1024) 
+        if not packet: break
+        data+=packet
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("Q",packed_msg_size)[0]
+    
+    while len(data) < msg_size:
+        data += client_socket.recv(4*1024)
+    frame_data = data[:msg_size]
+    data  = data[msg_size:]
+    frame = pickle.loads(frame_data)
+    cv2.imshow("RECEIVING VIDEO",frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key  == ord('q'):
+        break
+client_socket.close()
